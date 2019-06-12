923674ccdecdb28c72a7f38194157c22ff3df70a
William Moody <williammoody@Williams-MacBook-Air.local>
2019-06-12 17:04:15 -0600

    Completes StoryBoard

---
+++ MyCoolNewGameProject/GameScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameScreenViewController.swift
+//  MyCoolNewGameProject
+//
+//  Created by William Moody on 6/12/19.
+//  Copyright © 2019 Lo Howard. All rights reserved.
+//
+
+import UIKit
+
+class GameScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
