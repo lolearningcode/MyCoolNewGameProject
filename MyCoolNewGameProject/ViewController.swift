69cfb5d0097d32d8922e7b57c2345c3c7f571af9
Lo <lolearningcode@gmail.com>
2019-06-12 12:27:06 -0600

    initial commit

---
+++ MyCoolNewGameProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCoolNewGameProject
+//
+//  Created by Lo Howard on 6/12/19.
+//  Copyright © 2019 Lo Howard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
