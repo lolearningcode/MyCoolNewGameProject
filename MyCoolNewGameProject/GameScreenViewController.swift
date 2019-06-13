e755421071d7187d2f78d4c1f0d3645084b788af
William Moody <williammoody@Williams-MacBook-Air.local>
2019-06-13 09:09:21 -0600

    Completes button tapped logic sorta

--- MyCoolNewGameProject/GameScreenViewController.swift
+++ MyCoolNewGameProject/GameScreenViewController.swift
@@ -14,7 +14,28 @@ class GameScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activateButtons()
         
+    }
+    
+    func activateButtons() {
+        Buttons.forEach{$0.addTarget(self, action: #selector(buttonTapped(sender:)), for: .touchUpInside)}
+        for button in Buttons {
+            button.setImage(UIImage(named: ""), for: .normal)
+        }
+    }
+    
+    @objc func buttonTapped(sender: UIButton) {
+        print("tapped")
+        buttonLogic(sender)
+    }
+    
+    func buttonLogic(_ button: UIButton) {
+        //check if cell is taken
+        //swap image accordingly
+        //check for winner
+        //AI take their turn
+        //check for winner
     }
     
 }
